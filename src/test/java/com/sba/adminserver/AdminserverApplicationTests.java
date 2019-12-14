d9d9943b029ff4e4fed0a7b36f80bb8a7b331710
Dilip Nepali <er.dilip.n@gmail.com>
2019-12-13 21:18:02 -0500

    Spring Boot Admin Server

---
+++ src/test/java/com/sba/adminserver/AdminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sba.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
